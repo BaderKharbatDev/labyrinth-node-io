94130d3ef33fd67a3b9bd8afdf044217c9f7ae53
Bader Kharbat <bgkharba@ncsu.edu>
2021-07-06 18:37:54 -0400

    modularized and working

--- frontend/js/canvas.js
+++ frontend/js/canvas.js
@@ -1,12 +1,19 @@
 import {colors, units} from '/static/js/constants.js'
 import Board from '/game_logic/board.js'
 
-let canvas_id = 'canvas'
-let canvas_pixle_size = 600
-let board_length = 10
+export default class canvas {
+    constructor() {
+        let canvas_id = 'canvas'
+        let canvas_pixle_size = 600
+        let board_length = 10
+    
+        this.board = new Board(canvas_id, canvas_pixle_size, board_length)
+        this.board.colorBG(colors.white)
+        this.board.colorGrid(colors.red)
+    }
+}
 
-let board = new Board(canvas_id, canvas_pixle_size, board_length)
-board.colorBG(colors.white)
-board.colorGrid(colors.red)
+    
+
 
 
--- frontend/js/client_socket.js
+++ frontend/js/client_socket.js
@@ -1,4 +1,9 @@
+import Canvas from '/static/js/canvas.js'
 var socket = io.connect();
+
+let client_canvas = {}
+
 socket.on('connect', function(data) {
     console.log('Connected')
+    client_canvas = new Canvas() 
 });
