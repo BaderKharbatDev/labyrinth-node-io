cd08dac860ff873ef14351e3e7c99b5faba3c320
bgkharba <bgkharba>
2021-07-09 10:59:21 -0400

    .

--- game_logic/game.js
+++ game_logic/game.js
@@ -15,5 +15,52 @@ export default class Game {
         this.grid_size
         this.walls   
     }
-    
+
+    sleep(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }   
+
+    //Handles the game data per tick i.e. moving the player and checking for win condition
+    gameLoop() {
+        let tickRate = 1000/200
+        while(this.gameState = gameStates.INGAME) {
+            await this.sleep(tickRate)
+        }
+    }
+
+    //Handles sending the game data 
+    sendGameData() {
+        let tickRate = 1000/40
+        while(this.gameState = gameStates.INGAME) {
+            await this.sleep(tickRate)
+        }
+    }
+
+    handleUserData(socketID, KeyInputs) {
+        const previous_row_pos = this.players[socketID].row
+        const previous_col_pos = this.players[socketID].col
+        
+        this.players[socketID].keyinputs = KeyInputs
+        if(KeyInputs.left && !KeyInputs.right) { //left
+
+        } else if(!KeyInputs.left && KeyInputs.right) { //right
+
+        }
+        if(KeyInputs.up && !KeyInputs.down) { //up
+
+        } else if(!KeyInputs.up && KeyInputs.down) { //down
+
+        }
+
+        const current_row_pos = this.players[socketID].row
+        const current_col_pos = this.players[socketID].col
+        if(previous_col_pos == current_col_pos && previous_row_pos == current_row_pos) {
+            this.players[socketID].playerState = Players.playerStates.RUNNING
+        } else {
+            this.players[socketID].playerState = Players.playerStates.IDLE 
+        }
+    }
+   
 }
--- server/server_sockets.js
+++ server/server_sockets.js
@@ -1,12 +1,30 @@
 const Maze = require('../game_logic/maze.js')
 var map = {}
 const connections = []
+const games = []
 
 module.exports = function(io) {
 
     io.on('connection', (socket) => {
+
         addUser(socket)
         socket.emit('board-state', map)
+
+        //----------Handles User Entry Data i.e. before joining a lobby
+        socket.on('user-info-data', (data)=>{
+
+        })
+
+        //----------Handles starting the game
+        socket.on('game-start', (data)=>{
+            
+        })
+
+        //----------Handles User Input during a game
+        socket.on('user-input-data', (data)=>{
+
+        })
+
 
         socket.on('disconnect', ()=>{
             removeUser(socket.id)
