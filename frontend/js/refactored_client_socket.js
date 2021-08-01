e32523a7252e6ad21a9b4535b477ea99f06be708
Bader Kharbat <bgkharba@ncsu.edu>
2021-08-01 10:55:55 -0400

    .

--- frontend/js/refactored_client_socket.js
+++ frontend/js/refactored_client_socket.js
@@ -23,7 +23,7 @@ const client_constants = {
 
 // Create WebSocket connection.
 // const soc = new WebSocket('ws://localhost:80');
-var soc = new WebSocket('ws://localhost:443/', 'echo-protocol');
+var soc = new WebSocket('wss://localhost', 'echo-protocol');
 
 // Connection opened
 soc.addEventListener('open', function (event) {
